14d880db6003d049ec97bdff69a9a744c2da9429
Nadeem <nadeem.nsr03@yahoo.com>
2020-06-18 01:11:02 +0530

    Add Custom Message Class

---
+++ src/main/java/com/developerblog/app/ws/model/response/ErrorMessage.java
@@ -0,0 +1,15 @@
+package com.developerblog.app.ws.model.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorMessage {
+    private Date timestamp;
+    private String message;
+}
